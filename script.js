c43a1cfaac00e6c43b4d1c17062d3e1c37743a24
Dunja <dunjabeki@gmail.com>
2024-01-30 11:59:35 +0100

    Finished scroll bar, scroll up, active link

--- script.js
+++ script.js
@@ -87,3 +87,41 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+
+//Show scroll up
+
+const scrollUp = () => {
+  const scrollUp = document.getElementById('scroll-up');
+  this.scrollY >=350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+
+}
+
+window.addEventListener('scroll', scrollUp)
+
+
+//Scroll sections - Active link
+document.addEventListener('DOMContentLoaded', function () {
+  const sections = document.querySelectorAll('section[id]');
+
+  const scrollActive = () => {
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+      const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop - 58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']');
+
+      if (sectionClass) {
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+          sectionClass.classList.add('active-link');
+        } else {
+          sectionClass.classList.remove('active-link');
+        }
+      }
+    });
+  };
+
+  window.addEventListener('scroll', scrollActive);
+});
